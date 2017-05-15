3b1a969ca303bbec72ce9212c2b5fd9bc082ea2c
Chandrasekar Gokulanathan <g.chandrasekar1993@gmail.com>
2017-05-15 13:44:49 +0530

    Download Code POC

--- server/app/controller.js
+++ server/app/controller.js
@@ -1,10 +1,13 @@
 var cmd = require('node-cmd');
+var zipFolder = require('zip-folder');
 
 exports.buildApp = function(req, res, next){
 	console.log("Building the app");
 	 cmd.get(
         `
         cd template
+        ionic platform add android
+        ionic run android --device
         ionic serve
         `,
         function(err, data, stderr){
@@ -12,4 +15,14 @@ exports.buildApp = function(req, res, next){
         }
     );
     
-}
+};
+
+exports.downloadCode = function(req, res, next) {
+    zipFolder('template','appCode/App.zip', function(err){
+        if(!err) {
+            var file = "appCode/App.zip";
+            res.download(file);
+        }
+    });
+    
+};
--- server/app/routes.js
+++ server/app/routes.js
@@ -1,27 +1,7 @@
 var controller = require('./controller');
 module.exports = function(app) {
 
-	// server routes ===========================================================
-	// handle things like api calls
-	// authentication routes
-
-	// frontend routes =========================================================
-	// route to handle all angular requests
-	// app.get('*', function(req, res) {
-	// 	res.sendfile('./public/index.html');
-	// });
-
 	app.post('/buildApp', controller.buildApp);
-
+	app.get('/downloadCode', controller.downloadCode);
+	
 };
-
-
-/*
-var express = require('express');
-var router = express.Router();
-var controller = require('./controller');
-
-router.get('/buildApp', controller.buildApp);
-
-module.exports = router;
- */
