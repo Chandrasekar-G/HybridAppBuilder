4e5a102c768a3f9036ab09e5a17ec294bcedff05
Chandrasekar Gokulanathan <g.chandrasekar1993@gmail.com>
2017-05-16 18:19:07 +0530

    Resolved dependencies for node-cmd and zip-folder

--- server/app/routes.js
+++ server/app/routes.js
@@ -1,6 +1,7 @@
 var controller = require('./controller');
 module.exports = function(app) {
 
+	app.get('*', function(req, res) { res.sendfile('./public/index.html'); });
 	app.post('/buildApp', controller.buildApp);
 	app.get('/downloadCode', controller.downloadCode);
 	
