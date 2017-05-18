bb8ea67960a23b11f24817360fe27eff373bbf9e
Chandrasekar Gokulanathan <g.chandrasekar1993@gmail.com>
2017-05-18 12:31:12 +0530

    Service for single file upload

--- server/app/controller.js
+++ server/app/controller.js
@@ -1,5 +1,7 @@
 var cmd = require('node-cmd');
 var zipFolder = require('zip-folder');
+var ncp = require('ncp');
+var fs = require('fs');
 
 exports.buildApp = function(req, res, next){
 	console.log("Building the app");
@@ -26,3 +28,33 @@ exports.downloadCode = function(req, res, next) {
     });
     
 };
+
+exports.fileUpload = function(req, res, next) {
+    var source = "temp/",
+        destination = "uploads/" + req.body.filePath + "";
+
+        console.log(source + "--" + destination);
+  
+    
+    ncp(source, destination, function (err) {
+        if (err) {
+            return console.error(err);
+        }
+        rmDir(source, false);
+    });
+};
+
+rmDir = function(dirPath, removeSelf) {
+      if (removeSelf === undefined)
+        removeSelf = true;
+      try { var files = fs.readdirSync(dirPath); }
+      catch(e) { return; }
+      if (files.length > 0)
+        for (var i = 0; i < files.length; i++) {
+          var filePath = dirPath + '/' + files[i];
+          if (fs.statSync(filePath).isFile())
+            fs.unlinkSync(filePath);
+        }
+      if (removeSelf)
+        fs.rmdirSync(dirPath);
+    };
--- server/app/routes.js
+++ server/app/routes.js
@@ -1,8 +1,22 @@
 var controller = require('./controller');
+var multer  = require('multer')
+
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './temp');
+  },
+  filename: function (req, file, cb) {
+      cb(null, file.originalname);        
+  }
+});
+
+var upload = multer({ storage: storage });
+
 module.exports = function(app) {
 
 	app.get('*', function(req, res) { res.sendfile('./public/index.html'); });
 	app.post('/buildApp', controller.buildApp);
 	app.get('/downloadCode', controller.downloadCode);
+	app.post('/fileUpload',upload.single('image'), controller.fileUpload);
 	
 };
